3c984f2d14fdb0b27e9b1272521f56f154ffca3e
Fanny Dorthey <fannydor@ev-k6.kzn.21-school.ru>
2023-10-19 20:57:53 +0300

    dfhdfgzsdvarg

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -4,9 +4,7 @@ public class WordsBuilder {
     public void buildPhrase(char... chars) {
         int length = chars.length;
         int number = 0;
-        while (number < length) {
-            System.out.print(chars);
-        }
+        System.out.print(chars);
 
     }
 }
